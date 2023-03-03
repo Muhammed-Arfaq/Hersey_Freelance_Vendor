cf22477d7fc0f5e2071ee4502ac5edebd4ec7981
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-03 07:20:30 +0530

    updated

--- src/Redux/Store/store.jsx
+++ src/Redux/Store/store.jsx
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import EditGigReducer from '../Reducer/editGigModal'
 import EditProfileReducer from '../Reducer/editProfileModal'
 import gigReducer from '../Reducer/gigModal'
-import viewGigReducer from '../Reducer/ViewGigModal'
+import viewGigReducer from '../Reducer/viewGigModal'
 import viewOrderReducer from '../Reducer/viewOrderModal'
 
 export const store = configureStore({
