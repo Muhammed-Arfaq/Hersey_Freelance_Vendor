4ecf9ae15e2fe317686e7ded09c41011507b0ca0
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-03 12:03:16 +0530

    commit

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://13.233.255.36/"
+    // baseURL: "http://localhost:3500/"
+
+    baseURL: "https://herseymensformals.ml/"
+
+    // baseURL: "http://13.233.255.36/"
 })
 
 export default instance
