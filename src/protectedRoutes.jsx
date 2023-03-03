bdfc222763c488ff52cbca435993689171fc8d46
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-03 07:32:32 +0530

    updated

--- src/protectedRoutes.jsx
+++ src/protectedRoutes.jsx
@@ -1,22 +1,21 @@
 import { Navigate } from "react-router-dom"
-import { vendorAuth } from "../src/API"
 
 const ProtectedRoutes = ({ children }) => {
     const token = localStorage.getItem("jwt")
 
     if (token) {
         const vendorId = localStorage.getItem("vendorId")
-        vendorAuth(vendorId, token).then((result) => {
+        axios.get(`/vendor/vendorAuth/${vendorId}`, { headers: { Authorization: `Bearer ${token}` } })(vendorId, token).then((result) => {
             const status = result.data.userData.status
-            if(status == 'Blocked'){
+            if (status == 'Blocked') {
                 localStorage.clear();
                 window.location.reload()
             }
         })
     }
 
-    if(!token) {
-        return <Navigate to = {"/vendor/login"} replace = {true}></Navigate>
+    if (!token) {
+        return <Navigate to={"/vendor/login"} replace={true}></Navigate>
     }
     return children
 }
