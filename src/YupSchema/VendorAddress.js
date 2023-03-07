140daae1c9e67359113e01526cef4339d882801d
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-07 18:39:50 +0530

    updated

--- src/YupSchema/VendorAddress.js
+++ src/YupSchema/VendorAddress.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup'
 
 const regex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
-const addr = /^[a-zA-Z0-9\s,'-]*$/;
+const addr = /^[\w\s,'-]+(?:\s\([\w\s,'-]+\))?,\s[\w\s,'-]+(?:,\s[\w\s,'-]+)?$/;
 
 export const VendorAddress = yup.object().shape({
     country: yup.string().matches(regex, 'Country is not valid').required('country is required'),
