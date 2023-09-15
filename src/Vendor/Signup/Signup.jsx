1f4a3a710bc2e65c77db8e0208c68667dad0eaa5
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-09-15 12:29:54 +0530

    code updated

--- src/Vendor/Signup/Signup.jsx
+++ src/Vendor/Signup/Signup.jsx
@@ -371,7 +371,7 @@ export default function VendorSignup() {
                         className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-72 ease-linear transition-all duration-150"
                         type="Submit"
                       >
-                        Send OTP
+                        Create Account
                       </button>
                     </div>
                   </form>
